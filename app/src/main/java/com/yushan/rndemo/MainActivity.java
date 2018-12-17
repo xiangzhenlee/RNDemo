4aa609ac3811166fde39fcc8ff184d13ab23d1bd
lixz <lixz@wanbu.com.cn>
2018-12-17 14:13:58 +0800

    frist updata
    a demo for studying React Native

---
+++ app/src/main/java/com/yushan/rndemo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.yushan.rndemo;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactInstanceManager;
+
+public class MainActivity extends ReactActivity {
+
+    private String mCurrentLocale;
+
+    @Override
+    protected String getMainComponentName() {
+        return "HelloWorld";
+    }
+}
